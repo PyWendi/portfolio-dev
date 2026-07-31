e6cb32a0b5a5e0b4d895018b63c3dadaf9176dd7
PyWendi <rakotondranaivogilbert21@gmail.com>
2026-07-31 12:41:15 +0300

    correcting text for footer tooltip

--- src/layout/ContactSection.vue
+++ src/layout/ContactSection.vue
@@ -66,7 +66,7 @@ const { t } = useI18n()
                     <li>
                         <OutsideLink 
                         link="https://linkedin.com/in/anderson-rakotondranaivo-b7b466361" 
-                        :tooltip_text="t('sections.contact.links.linkedin_tooltip')">
+                        :tooltip_text="t('sections.header.links.linkedin_tooltip')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
                             class="lucide lucide-linkedin-icon lucide-linkedin w-[15px] md:w-[17px] lg:w-[20px] xl:w-[24px]"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
                         </OutsideLink>
@@ -75,7 +75,7 @@ const { t } = useI18n()
                     <li>
                         <OutsideLink
                         link="https://github.com/PyWendi"
-                        :tooltip_text="t('sections.contact.links.github_tooltip')">
+                        :tooltip_text="t('sections.header.links.github_tooltip')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
                             class="lucide lucide-github-icon lucide-github w-[15px] md:w-[17px] lg:w-[20px] xl:w-[24px]"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
                         </OutsideLink>
@@ -84,7 +84,7 @@ const { t } = useI18n()
                     <li>
                         <OutsideLink
                         link="rakotondranaivogilbert21@gmail.com"
-                        :tooltip_text="t('sections.contact.links.email_tooltip')"
+                        :tooltip_text="t('sections.header.links.email_tooltip')"
                         :is_link_to_copy="true">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
                             class="lucide lucide-mail-icon lucide-mail w-[15px] md:w-[17px] lg:w-[20px] xl:w-[24px]"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
@@ -94,7 +94,7 @@ const { t } = useI18n()
                     <li>
                         <OutsideLink
                         link="https://www.facebook.com/PyWendi21"
-                        :tooltip_text="t('sections.contact.links.facebook_tooltip')">
+                        :tooltip_text="t('sections.header.links.facebook_tooltip')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
                             class="lucide lucide-facebook-icon lucide-facebook w-[15px] md:w-[17px] lg:w-[20px] xl:w-[24px]"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
                         </OutsideLink>
@@ -103,7 +103,7 @@ const { t } = useI18n()
                     <li>
                         <OutsideLink
                         link="https://discord.com/users/972090595881390150"
-                        :tooltip_text="t('sections.contact.links.discord_tooltip')">
+                        :tooltip_text="t('sections.header.links.discord_tooltip')">
                             <img 
                             class="w-[20px] md:w-[70%] lg:w-[70%] xl:w-[80%]"
                             src="../assets/img/Discord.svg" alt="Discord svg">
@@ -113,7 +113,7 @@ const { t } = useI18n()
                     <li>
                         <OutsideLink
                         link="https://zodiak21.itch.io"
-                        :tooltip_text="t('sections.contact.links.itch_tooltip')">
+                        :tooltip_text="t('sections.header.links.itch_tooltip')">
                             <img 
                             class="w-[20px] md:w-[70%] lg:w-[70%] xl:w-[85%]"
                             src="../assets/img/itch_io.svg" alt="Itch svg">
