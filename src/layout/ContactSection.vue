02b1e8a704d85cf79b2d2485b24ac75c7f52b46b
PyWendi <rakotondranaivogilbert21@gmail.com>
2026-08-24 23:47:58 +0300

    update : updating linkeding link, wrong link pushed to prod

--- src/layout/ContactSection.vue
+++ src/layout/ContactSection.vue
@@ -65,7 +65,7 @@ const { t } = useI18n()
                     <!-- Linkedin -->
                     <li>
                         <OutsideLink 
-                        link="https://linkedin.com/in/anderson-rakotondranaivo-b7b466361" 
+                        link="https://www.linkedin.com/in/joyaux-wendi-anderson-rakotondranaivo-gilbert-b7b466361/" 
                         :tooltip_text="t('sections.header.links.linkedin_tooltip')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
                             class="lucide lucide-linkedin-icon lucide-linkedin w-[15px] md:w-[17px] lg:w-[20px] xl:w-[24px]"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
